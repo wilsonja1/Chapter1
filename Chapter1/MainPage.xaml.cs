d319c512e9e135494869e8a9d235e3a2d39c18af
Jack Wilson <battlejackmaster@gmail.com>
2018-06-29 12:06:28 -0700

    human is now draggable. pg 45

--- Chapter1/MainPage.xaml.cs
+++ Chapter1/MainPage.xaml.cs
@@ -90,6 +90,14 @@ namespace Chapter1
             AnimateEnemy(enemy, 0, playArea.ActualWidth - 100, "(Canvas.Left)");
             AnimateEnemy(enemy, random.Next((int)playArea.ActualHeight - 100), random.Next((int)playArea.ActualHeight - 100), "(Canvas.Top)");
             playArea.Children.Add(enemy);
+
+            enemy.PointerEntered += Enemy_PointerEntered;
+        }
+
+        private void Enemy_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            if (humanCaptured)
+                EndTheGame();
         }
 
         private void AnimateEnemy(ContentControl enemy, double from, double to, string propertyToAnimate)
@@ -105,6 +113,55 @@ namespace Chapter1
             Storyboard.SetTargetProperty(animation, propertyToAnimate);
             storyboard.Children.Add(animation);
             storyboard.Begin();
+        }
+
+        private void Human_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            if (enemyTimer.IsEnabled)
+            {
+                humanCaptured = true;
+                Human.IsHitTestVisible = false;
+            }
+        }
+
+        private void target_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            if(targetTimer.IsEnabled && humanCaptured)
+            {
+                progressBar.Value = 0;
+                Canvas.SetLeft(target, random.Next(100, (int)playArea.ActualWidth - 100));
+                Canvas.SetTop(target, random.Next(100, (int)playArea.ActualHeight - 100));
+                Canvas.SetLeft(Human, random.Next(100, (int)playArea.ActualWidth - 100));
+                Canvas.SetTop(Human, random.Next(100, (int)playArea.ActualHeight - 100));
+                humanCaptured = false;
+                Human.IsHitTestVisible = true;
+            }
+        }
+
+        private void playArea_PointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            if (humanCaptured)
+            {
+                Point pointerPosition = e.GetCurrentPoint(null).Position;
+                Point relativePosition = grid.TransformToVisual(playArea).TransformPoint(pointerPosition);
+                if ((Math.Abs(relativePosition.X - Canvas.GetLeft(Human)) > Human.ActualWidth * 3)
+                    || (Math.Abs(relativePosition.Y - Canvas.GetTop(Human)) > Human.ActualWidth * 3))
+                {
+                    humanCaptured = false;
+                    Human.IsHitTestVisible = true;
+                }
+                else
+                {
+                    Canvas.SetLeft(Human, relativePosition.X - Human.ActualWidth / 2);
+                    Canvas.SetTop(Human, relativePosition.Y - Human.ActualHeight / 2);
+                }
+            }
+        }
+
+        private void playArea_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            if (humanCaptured)
+                EndTheGame();
         }
     }
 }
